65f26e74590d4b9eeea4b7c7ce060b6578d51437
Fu-Lin Peng <fulinpeng53@gmail.com>
2024-08-07 18:53:35 -0400

    Completed Loop Exercise

---
+++ IterationStatements/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i<5; i++)
+            {
+                Console.WriteLine(i); 
+            }
+            for(int i= 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i++);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
